74e7cfc235901e02283b359d1a44fa260419ff5b
Chris.C.Chou <chrischou@intumit.com>
2018-11-16 11:49:02 +0800

    upload img and show

--- module/requestHandlers.js
+++ module/requestHandlers.js
@@ -1,5 +1,6 @@
 var queryString = require("querystring"),
-    fs = require("fs");
+    fs = require("fs"),
+    formidable = require("formidable");
 
 function start(response) {
     console.log("Request handler 'start' was called.");
@@ -10,9 +11,9 @@ function start(response) {
     'charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
-    '<input type="submit" value="Submit text" />'+
+    '<form action="/upload" enctype="multipart/form-data" method="post">'+
+    '<input type="file" name="upload">' +
+    '<input type="submit" value="Upload file" />'+
     '</form>'+
     '</body>'+
     '</html>';
@@ -22,11 +23,22 @@ function start(response) {
     response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
     console.log("Request handler 'upload' was called.");
-    response.writeHead(200, {"Content-Type" : "text/plain"});
-    response.write("You've sent: " + queryString.parse(postData).text);
-    response.end();
+
+    var form = new formidable.IncomingForm();
+    form.uploadDir = "tmp"; // cause default upload dir to rename will not permitted
+
+    console.log("about to parse");
+    form.parse(request, function (error, fields, files) {
+        console.log("parsing done");
+        fs.renameSync(files.upload.path, "./tmp/test.png");
+        response.writeHead(200, {"Content-Type" : "text/html"});
+        response.write("Received image:</br>");
+        response.write("<img src='/show' />");
+        response.end();
+    })
+    
 }
 
 function show(response, postData) {
--- module/router.js
+++ module/router.js
@@ -1,7 +1,7 @@
-function route(handle, pathName, response, postData) {
+function route(handle, pathName, response, request) {
     console.log("About to route a request for " + pathName);
     if (typeof handle[pathName] === 'function') {
-        return handle[pathName](response, postData);
+        return handle[pathName](response, request);
     } else {
         console.log("No request handler found for " + pathName);
         response.writeHead(404, {"Content-Type" : "text/plain"});
