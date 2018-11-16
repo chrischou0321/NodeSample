c4c566875ca40a9131db8b154d17339f23fb6aa9
Chris.C.Chou <chrischou@intumit.com>
2018-11-16 10:57:01 +0800

    get query string input practice

--- module/server.js
+++ module/server.js
@@ -3,10 +3,18 @@ var url = require("url");
 
 function start(route, handle) {
     function onRequest(request, response) {
+        var postData = "";
         var pathName = url.parse(request.url).pathname;
         console.log("Request for " + pathName + " received.");
-        
-        route(handle, pathName, response);
+
+        request.setEncoding("utf8");
+        request.addListener("data", function (postDataChunk) {
+            postData += postDataChunk;
+            console.log("Received POST data chunk '" + postDataChunk + "', ");
+        });
+        request.addListener("end", function () {
+            route(handle, pathName, response, postData);
+        })
     }
     
     http.createServer(onRequest).listen(8888);
